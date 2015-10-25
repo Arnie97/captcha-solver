28703eb3ba5db219878490f1125b865e345bdffc
Arnie97 <arnie97@gmail.com>
2015-10-25 21:18:43 +0800

    Reorganize unit tests with JSON config files.

--- tests/test_captcha.py
+++ tests/test_captcha.py
@@ -1,15 +1,37 @@
 import os
 import nose
-from captcha import image_filter, solve, solve_url
+import json
+import captcha
 
 
-def test_captcha_solver():
-    path = 'tests/samples/'
+def load_json_config(function):
+    def test_case():
+        path = 'tests/templates/'
+        for file in os.listdir(path):
+            if file.rsplit('.', 1)[-1] == 'json':
+                with open(path + file) as f:
+                    params = json.load(f)
+                    function(params)
+    test_case.__name__ = function.__name__
+    return test_case
+
+
+@load_json_config
+def test_captcha_solver(params):
+    path = params.get('samples')
+    if path is None:
+        return
     for file in os.listdir(path):
-        assert solve(image_filter(path + file), 'numbers.bmp') == file[:6]
+        image = captcha.image_filter(path + file, params)
+        result = captcha.solve(image, params)
+        assert ''.join(map(str, result)) == file.rsplit('.', 1)[0]
 
 
-def test_get_image_by_url():
-    image_url = 'http://www.afreesms.com/image.php'
-    for i in range(10):
-        assert len(solve_url(image_url, 'numbers.bmp')) == 6
+@load_json_config
+def test_get_image_by_url(params):
+    for i in range(3):
+        image_data = captcha.fetch(params['url'])
+        image = captcha.image_filter(image_data, params)
+        result = captcha.solve(image, params)
+        assert len(result) == params.get('length', 4)
+        assert all(x is not None for x in result)
